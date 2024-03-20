082e225317d84c4cdbe0e28f99c44910499ec5ea
Ven <venchupikov@gmail.com>
2024-03-20 13:52:45 +0300

    Snake game for M5stickC Plus2

--- M5StickC/UIFlow/Snake/README.md
+++
@@ -1,3 +0,0 @@
-# Classic Snake game for M5StickC 
-![Demo](https://i.imgur.com/pdz0iQS.gif)  
-Eat as much food as you can, don't hit the borders and your tail. The snake can be controlled by a human or by an AI.
--- Snake.py
+++ Snake.py
@@ -1,12 +1,15 @@
-from m5stack import lcd, btnA, btnB
+# Created by remixer-dec and modify for M5stickC Plus2 by krkwsmk
+
+from M5 import Lcd
 from machine import Timer
 from random import randint, seed
 from utime import ticks_cpu
+from hardware import *
     
 class Food:
     pos = [0, 0]
     def generate():
-        Food.pos = [randint(2, 157), randint(2, 77)]
+        Food.pos = [randint(2, 237), randint(2, 131)]
         Food.pos = [Food.pos[0] if Food.pos[0] %2 == 0 else Food.pos[0] - 1, Food.pos[1] if Food.pos[1] % 2 == 0 else Food.pos[1] - 1]
         Food.checkPosition()
         
@@ -27,17 +30,18 @@ class Snake:
     notail = [2,2]
     isDead = False
     def isAlive(head):
-        return head[0] > 0 and head[0] < 158 and head[1] > 0 and head[1] < 78 and Snake.tail.count(head) == 0
+        return head[0] > 0 and head[0] < 238 and head[1] > 0 and head[1] < 132 and Snake.tail.count(head) == 0
     
     def run():
         seed(ticks_cpu())
-        lcd.setRotation(3)
+        Lcd.setRotation(3)
         Snake.timer.init(period=Snake.speed, mode=Timer.PERIODIC, callback=Snake.move)
         
     def dirChange():
-        if btnB.isPressed():
+        M5.update()
+        if BtnB.wasPressed():
             Snake.dr += 1
-        if btnA.isPressed():
+        if BtnA.wasPressed():
             Snake.dr -= 1
         if Snake.dr > 3:
             Snake.dr = 0
@@ -58,7 +62,7 @@ class Snake:
     def move(tmr=None):
         head = Snake.tail[0].copy()
         Snake.dirChange()
-        #Snake.basicAIMove(head) #uncomment this line if you want AI to play Snake
+        # Snake.basicAIMove(head) #uncomment this line if you want AI to play Snake
         head[0] += Snake.moves[Snake.dr][0]
         head[1] += Snake.moves[Snake.dr][1]
         if [head].count(Food.pos) == 0:
@@ -74,41 +78,46 @@ class Snake:
             Snake.dr = 2 if Food.pos[1] > head[1] and Snake.dr != 0 else 0 if Snake.dr !=2 else Snake.dr
         if head[1] == Food.pos[1]:
             Snake.dr = 1 if Food.pos[0] > head[0] and Snake.dr != 3 else 3 if Snake.dr !=1 else Snake.dr
-        if (head[0] > 155 and Snake.dr == 1) or (head[0] < 3 and Snake.dr == 3):
+        if (head[0] > 235 and Snake.dr == 1) or (head[0] < 3 and Snake.dr == 3):
             print(head[0])
             Snake.dr = 0 if head[1] > Food.pos[1] else 2
-        if (head[1] > 75 and Snake.dr == 2) or (head[1] < 4 and Snake.dr == 0):
+        if (head[1] > 129 and Snake.dr == 2) or (head[1] < 4 and Snake.dr == 0):
             Snake.dr = 3 if head[0] > Food.pos[0] else 1
 
 class Game:
     def drawBorders():
-        lcd.drawRect(0, 0, 160, 80)
+        Lcd.drawRect(0, 1, 239, 133, 0xffd700)
         
     def prepareField():
         Snake.run()
-        lcd.clear()
+        Lcd.clear()
         Game.drawBorders()
         Food.generate()
         
     def loop():
         #render snake
         for tailpart in Snake.tail:
-            lcd.fillRect(tailpart[0], tailpart[1], 2, 2)
+            Lcd.fillRect(tailpart[0], tailpart[1], 2, 2)
         #clear old tail coords
-        lcd.fillRect(Snake.notail[0], Snake.notail[1], 2, 2, 0)
+        Lcd.fillRect(Snake.notail[0], Snake.notail[1], 2, 2, 0)
         #render food
-        lcd.fillRect(Food.pos[0], Food.pos[1], 2, 2, 0x00ff00)
+        Lcd.fillRect(Food.pos[0], Food.pos[1], 2, 2, 0xffd700)
         return not Snake.isDead
     
     def over():
         Snake.timer.deinit()
-        lcd.clear()
-        lcd.text(lcd.CENTER, lcd.CENTER, "GAME OVER")
-        lcd.text(lcd.CENTER, 10, "score: " + repr(len(Snake.tail)))
-        while btnA.isPressed():
+        Lcd.clear()
+        Lcd.setCursor(25, 40)
+        Lcd.setTextSize(3)
+        Lcd.print("GAME OVER")
+        Lcd.setCursor(65, 70)
+        Lcd.setTextSize(2)
+        Lcd.print("Score: " + repr(len(Snake.tail)))
+        while BtnA.wasPressed():
             pass
         while True:
-            if btnA.isPressed():
+            M5.update()
+            if BtnA.wasPressed():
                 Snake.reset()
                 Game.prepareField()
                 return 0
@@ -116,4 +125,3 @@ class Game:
 Game.prepareField()
 while True:
     Game.loop() or Game.over()
-
